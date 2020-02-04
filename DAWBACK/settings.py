51495b780f430dd78c91be2fc0ce678c3ba3bbea
chrallar <chrallar@espol.edu.ec>
2020-02-04 00:34:04 -0500

    intento

--- DAWBACK/settings.py
+++ DAWBACK/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ju#ic8y$7f^yo0fz8e%o8bo2=gn91h_1u7bh_jfv-_+x1xs-zg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1','https://dawfcsh.herokuapp.com']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1','dawfcsh.herokuapp.com']
 
 
 # Application definition
