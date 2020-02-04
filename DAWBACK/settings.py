cce250e19937a7582771f0061c5cf1bdca064560
chrallar <chrallar@espol.edu.ec>
2020-02-03 23:35:57 -0500

    whitenoise settings

--- DAWBACK/settings.py
+++ DAWBACK/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'ju#ic8y$7f^yo0fz8e%o8bo2=gn91h_1u7bh_jfv-_+x1xs-zg'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','https://dawfcsh.herokuapp.com']
 
 
 # Application definition
@@ -118,9 +118,9 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+
+
+
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
