032287f9a4a7b46c00aa9e8761d37e05129b7679
chrallar <chrallar@espol.edu.ec>
2020-02-04 15:28:00 -0500

    Creación de modelos

--- repositorio/admin.py
+++ repositorio/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import *
 # Register your models here.
 admin.site.register(Post)
+admin.site.register(Usuario)
+admin.site.register(Noticia)
+admin.site.register(Rol)
+admin.site.register(Materia)
+admin.site.register(Carrera)
+admin.site.register(Registro)
+admin.site.register(Termino)
+admin.site.register(Descarga)
--- repositorio/models.py
+++ repositorio/models.py
@@ -1,6 +1,49 @@
 from django.db import models
 from django.utils import timezone
 # Create your models here.
+class Rol(models.Model):
+    idRol=models.AutoField(primary_key=True)
+    nombreRol=models.CharField(max_length=200)
+
+class Usuario(models.Model):
+    idUsuario=models.AutoField(primary_key=True)
+    rol=models.ForeignKey(Rol,on_delete=models.CASCADE)
+    nombreCliente= models.CharField(max_length=500)
+    usuario=models.CharField(max_length=200)
+    password=models.CharField(max_length=200)
+    estado=models.BooleanField()
+    correo=models.CharField(max_length=500)
+class Noticia(models.Model):
+    idNoticia=models.AutoField(primary_key=True)
+    usuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    nombreNoticia=models.CharField(max_length=500)
+    descrNoticia=models.CharField(max_length=500)
+    creadoFecha = models.DateTimeField(default=timezone.now)
+
+class Carrera(models.Model):
+    idCarrera=models.AutoField(primary_key=True)
+    nombreCarrera=models.CharField(max_length=500)
+
+class Termino(models.Model):
+    idTermino=models.AutoField(primary_key=True)
+    nombreTermino=models.CharField(max_length=500)
+
+class Materia(models.Model):
+    idMateria=models.AutoField(primary_key=True)
+    carrera=models.ForeignKey(Carrera,on_delete=models.CASCADE)
+    nombreMateria=models.CharField(max_length=500)
+
+class Registro(models.Model):
+    idRegistro=models.AutoField(primary_key=True)
+    termino=models.ForeignKey(Termino,on_delete=models.CASCADE)
+    usuario=models.ForeignKey(Usuario,on_delete=models.CASCADE)
+    materia=models.ForeignKey(Materia,on_delete=models.CASCADE)
+
+class Descarga(models.Model):
+    idDescarga=models.AutoField(primary_key=True)
+    idRegistro = models.ForeignKey(Registro,on_delete=models.CASCADE)
+    fecha = models.DateTimeField(default=timezone.now)
+
 class Post(models.Model):
     author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
