3f27229597cf0f33f55ccdc79a5cc123e27eac4f
chrallar <chrallar@espol.edu.ec>
2020-02-05 05:32:16 -0500

    push rest noticia

--- repositorio/serializers.py
+++ repositorio/serializers.py
@@ -3,39 +3,39 @@ from repositorio.models import *
 class RolSerializer(serializers.ModelSerializer):
     class Meta:
         model=Rol
-        fields=('idRol','nombreRol')
+        fields=('id','idRol','nombreRol')
 class UsuarioSerializer(serializers.ModelSerializer):
     class Meta:
         model=Usuario
-        fields=('idUsuario','rol','nombreCliente','usuario','password','estado','correo')
+        fields=('id','idUsuario','rol','nombreCliente','usuario','password','estado','correo')
 
 class NoticiaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Noticia
-        fields=('idNoticia','usuario','nombreNoticia','descrNoticia','creadoFecha')
+        fields=('id','idNoticia','usuario','nombreNoticia','descrNoticia','creadoFecha')
 
 class CarreraSerializer(serializers.ModelSerializer):
     class Meta:
         model=Carrera
-        fields=('idCarrera','nombreCarrera')
+        fields=('id','idCarrera','nombreCarrera')
 
 class TerminoSerializer(serializers.ModelSerializer):
     class Meta:
         model=Termino
-        fields=('idTermino','nombreTermino')
+        fields=('id','idTermino','nombreTermino')
 
 class MateriaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Materia
-        fields=('idMateria','carrera','nombreMateria')
+        fields=('id','idMateria','carrera','nombreMateria')
 
 class RegistroSerializer(serializers.ModelSerializer):
     class Meta:
         model=Registro
-        fields=('idRegistro','termino','usuario','materia')
+        fields=('id','idRegistro','termino','usuario','materia')
 
 class DescargaSerializer(serializers.ModelSerializer):
     class Meta:
         model=Descarga
-        fields=('idDescarga','idRegistro','fecha')
+        fields=('id','idDescarga','idRegistro','fecha')
 
