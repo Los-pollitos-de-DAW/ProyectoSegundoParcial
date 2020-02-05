0bc0090e8e401193dc98e17ee9f6a5c0c3928658
chrallar <chrallar@espol.edu.ec>
2020-02-05 00:53:56 -0500

    Pruebas en django rest framework

--- DAWBACK/settings.py
+++ DAWBACK/settings.py
@@ -40,8 +40,9 @@ INSTALLED_APPS = [
     'repositorio.apps.RepositorioConfig',
     'rest_framework',
 ]
-
+CORS_ORIGIN_ALLOW_ALL=True
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
