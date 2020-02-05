8d92d911f200d5d88c50f5cc99b2e71e459042c1
luanvill <luanvill@espol.edu.ec>
2020-02-05 04:36:51 -0500

    Mouse herramienta eliminada :v

--- borrar/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\andres\documents\github\proyectosegundoparcial\borrar\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
